7dd2e1eae81cb6b2aa1edded216120c1f5db56ef
Jared Hancock <gravydish@gmail.com>
2022-02-14 12:29:51 -0600

    Project 1: Build a simple KV server/client pair

---
+++ src/client.py
@@ -0,0 +1,94 @@
+import asyncio
+from urllib import request
+
+async def gettime(host=None, port=12345):
+    reader, writer = await asyncio.open_connection(host or 'localhost', port)
+    time = await reader.readline()
+    time = time.decode('utf-8').rstrip()
+    print('Current time is:', time)
+
+from message import send_message, receive_message
+
+async def echo(host='localhost', port=12346):
+    reader, writer = await asyncio.open_connection(host or 'localhost', port)
+    while True:
+        try:
+            message = input('Say > ')
+            if not message:
+                break
+            send_message(writer, message)
+            print('Received >', await receive_message(reader))
+        except (EOFError, KeyboardInterrupt):
+            break
+
+import pickle
+    
+class StoreClient:
+    intro = "Welcome to the key-value storage system"
+    prompt = "(kv) "
+
+    def __init__(self, server, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reader, self.writer = server
+
+    async def request(self, cmd, *args):
+        send_message(self.writer, pickle.dumps((cmd, *args)))
+        return await receive_message(self.reader)
+
+    async def do_get(self, key):
+        response = await self.request('get', key)
+        if response is not None:
+            print(f"{key} => {response}")
+
+        else:
+            print(f"{key} not in the store")
+
+    async def do_set(self, key, value):
+        response = await self.request('set', key, value)
+        print(response)
+
+    async def do_del(self, key):
+        response = await self.request('del', key)
+        print(response)
+
+    async def cmdloop(self):
+        print(self.intro)
+        while True:
+            try:
+                cmd = input(self.prompt)
+            except (EOFError, KeyboardInterrupt):
+                break
+
+            cmd, *parts = cmd.split(' ')
+            try:
+                handler = getattr(self, 'do_' + cmd)
+                await handler(*parts)
+            except AttributeError:
+                print(f"Oops: {cmd} is not a supported command")
+            except TypeError as e:
+                print(f"Oops: {e}")
+
+async def kvclient(host='localhost', port=12347):
+    reader, writer = await asyncio.open_connection(host or 'localhost', port)
+    try:
+        handler = StoreClient(server=(reader, writer))
+        await handler.cmdloop()
+    except:
+        raise
+
+
+### Command-line interface to run the clients
+
+clients = {
+    'time': gettime,
+    'echo': echo,
+    'kv': kvclient,
+}
+
+import argparse
+parser = argparse.ArgumentParser("RAFT :: Warmup Exercise")
+parser.add_argument("client", choices=clients.keys())
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    asyncio.run(clients[args.client]())
---
+++ src/server.py
@@ -0,0 +1,94 @@
+import asyncio
+from inspect import ClosureVars
+import time
+
+async def timeserver(host='localhost', port=12345):
+    async def handle_client(reader, writer):
+        print("Connection from:", writer.get_extra_info('peername'))
+        msg = time.ctime()
+        writer.write(msg.encode('utf-8'))
+        writer.write("\n".encode('utf-8'))
+
+    server = await asyncio.start_server(handle_client, host, port)
+    print(f"Listening on {host}:{port}")
+    await server.serve_forever()
+
+from message import send_message, receive_message
+
+async def echoserver(host='localhost', port=12346, middleware=[]):
+    async def handler(reader, writer):
+        print("Connection from:", writer.get_extra_info('peername'))
+        try:
+            while True:
+                message = await receive_message(reader)
+                for F in middleware:
+                    message = F(message) or message
+                send_message(writer, message)
+        except asyncio.IncompleteReadError:
+            # Client likely disconnected
+            pass
+
+    server = await asyncio.start_server(handler, host, port)
+    print(f"Listening on {host}:{port}")
+    await server.serve_forever()
+
+async def echoserver_upper(host='localhost', port=12346):
+    return await echoserver(host, port, [lambda x: x.upper()])
+
+from store import KeyValueStore
+import pickle
+async def kvserver(host='localhost', port=12347):
+    store = KeyValueStore("/tmp/kvstore.db")
+
+    async def handler(reader, writer):
+        async def do_get(key):
+            if key not in store:
+                return None
+            return store.get(key)
+
+        async def do_set(key, value):
+            await store.set(key, value)
+            return True
+
+        async def do_del(key):
+            await store.delete(key)
+            return True
+
+        print("Connection from:", writer.get_extra_info('peername'))
+        try:
+            while True:
+                message = await receive_message(reader)
+                cmd_name, *args = pickle.loads(message)
+                try:
+                    cmd = locals()['do_' + cmd_name]
+                    send_message(writer, await cmd(*args))
+                except AttributeError:
+                    send_message(writer, False)    
+                
+        except asyncio.IncompleteReadError:
+            # Client likely disconnected
+            pass
+
+    server = await asyncio.start_server(handler, host, port)
+    print(f"Listening on {host}:{port}")
+    await server.serve_forever()
+
+
+
+
+### Command-line interface to run the servers
+
+servers = {
+    'time': timeserver,
+    'echo': echoserver,
+    'echoup': echoserver_upper,
+    'kv': kvserver,
+}
+
+import argparse
+parser = argparse.ArgumentParser("RAFT :: Warmup Exercise")
+parser.add_argument("server", choices=servers.keys())
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    asyncio.run(servers[args.server]())
